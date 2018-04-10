94efb71da8eef2c5080ea498e843ebb82a229089
Zidooer <zidooer@zidoo.tv>
2018-04-10 09:47:04 +0800

    add play time count

--- ZidooDigitalDemo/src/com/zidoo/test/hdmi/video/HdmiAndVideoActivity.java
+++ ZidooDigitalDemo/src/com/zidoo/test/hdmi/video/HdmiAndVideoActivity.java
@@ -7,6 +7,8 @@ package com.zidoo.test.hdmi.video;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.TextureView;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,6 +16,7 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.zuishare_test.R;
 import com.zidoo.test.hdmi.ZidooHdmiDisPlay;
@@ -23,7 +26,11 @@ public class HdmiAndVideoActivity extends Activity implements OnClickListener {
 	private WebView				mWebView		= null;
 	private ZidooHdmiDisPlay	mRealtekeHdmi	= null;
 	private boolean				isHdmiAudio		= true;
-
+	private TextView			mTimeCountView	= null;
+	private Handler				mHandler		= null;
+	private final static int	COUNT			= 0;
+	private final static int	COUNTTIME		= 1*1000;
+	private long mCount = 0 ;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -34,6 +41,7 @@ public class HdmiAndVideoActivity extends Activity implements OnClickListener {
 	private void initView() {
 		mPlayVideoTool = new PlayVideoTool((VideoView) findViewById(R.id.home_ac_videoview));
 		findViewById(R.id.sound_hdmi).setOnClickListener(this);
+		mTimeCountView = (TextView) findViewById(R.id.home_ac_time);
 		mWebView = (WebView) findViewById(R.id.webView);
 		mWebView.loadUrl("http://www.zidoo.tv");
 		mWebView.setWebViewClient(new WebViewClient() {
@@ -46,12 +54,61 @@ public class HdmiAndVideoActivity extends Activity implements OnClickListener {
 		});
 
 		ViewGroup hdmiGroud = (ViewGroup) findViewById(R.id.home_ac_hdmi);
-		mRealtekeHdmi = new ZidooHdmiDisPlay(HdmiAndVideoActivity.this, hdmiGroud, null,ZidooHdmiDisPlay.TYPE_SURFACEVIEW);
+		mRealtekeHdmi = new ZidooHdmiDisPlay(HdmiAndVideoActivity.this, hdmiGroud, null, ZidooHdmiDisPlay.TYPE_SURFACEVIEW);
 		mRealtekeHdmi.startDisPlay();
+		initCount();
 	}
 
+	private void initCount() {
+		mHandler = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+				switch (msg.what) {
+				case COUNT:
+					mHandler.removeMessages(COUNT);
+					mCount++;
+					mTimeCountView.setText(formatTime(mCount));
+					mHandler.sendEmptyMessageDelayed(COUNT, COUNTTIME);
+					break;
+
+				default:
+					break;
+				}
+			}
+		};
+		mHandler.sendEmptyMessage(COUNT);
+	}
+
+	private String formatTime(long time) {
+		String timeStr = "";
+		try {
+			long h = time / 3600;
+			long min = (time % 3600) / 60;
+			long ss = time % 60;
+			if (h < 10) {
+				timeStr = "0" + h;
+			} else {
+				timeStr = "" + h;
+			}
+			if (min < 10) {
+				timeStr = timeStr + ":0" + min;
+			} else {
+				timeStr = timeStr + ":" + min;
+			}
+			if (ss < 10) {
+				timeStr = timeStr + ":0" + ss;
+			} else {
+				timeStr = timeStr + ":" + ss;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return timeStr;
+	}
+	
 	@Override
 	protected void onDestroy() {
+		mHandler.removeMessages(COUNT);
 		mPlayVideoTool.onDestroy();
 		if (mRealtekeHdmi != null) {
 			mRealtekeHdmi.exit();
